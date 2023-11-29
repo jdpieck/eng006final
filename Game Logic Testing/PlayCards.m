590fa9ff3cab5ec9fac3ba768a64a1bce9d46f8e
jdpieck <jdpieck@ucdavis.edu>
2023-11-28 21:22:31 -0800

    Play Card Logic Updated

--- Game Logic Testing/PlayCards.m
+++ Game Logic Testing/PlayCards.m
@@ -1,7 +1,38 @@
-function [outputArg1,outputArg2] = PlayCards(inputArg1,inputArg2)
+function newPlayedCards = PlayCards(selectedCards, playedCards)
 %PLAYCARDS Summary of this function goes here
 %   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+    setSize = length(selectedCards);
+    selectedCardValue = mod(selectedCards, 13);
+    playedCardValue = mod(playedCards, 13);
+    
+    %Check if cards played are of matching value
+    if setSize > 1
+        for i = 1:(setSize-1)
+            if not(selectedCardValue(i) == selectedCardValue(i+1))
+                fprintf("Cards are not of matching value \n")
+                return
+            end
+        end
+    end
+
+    %Check if first player
+    if isempty(playedCards)
+        newPlayedCards = selectedCards;
+        return
+
+    end
+    
+    %Check if number of cards played matches previous play
+    if not(setSize == length(playedCards))
+       fprintf("Does not match set size \n")
+       return
+    end
+    
+    %Check if cards played are higher value
+    if selectedCardValue(1) > playedCardValue(1)
+        newPlayedCards = selectedCards;
+    else
+        fprintf("Value of cards not high enough \n")
+    end
 end
 
