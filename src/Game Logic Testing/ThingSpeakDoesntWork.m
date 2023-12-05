a1206ef4ed90ff540bb3cb620311ab9da1c6018b
jdpieck <jdpieck@ucdavis.edu>
2023-12-05 14:44:11 -0800

    Added Code for trouble shooting

--- src/Game Logic Testing/ThingSpeakDoesntWork.m
+++ src/Game Logic Testing/ThingSpeakDoesntWork.m
@@ -3,6 +3,9 @@ channelID = 2368599;
 readKey = 'LLHWWTB3K780X5S1';
 writeKey = 'XH18T2Q11AQPS9PA';
 
+url = sprintf('https://api.thingspeak.com/channels/%s/feeds.json?api_key=%s', num2str(channelID), userAPIKey);
+webwrite(url, weboptions('RequestMethod','delete'));
+
 thingSpeakWrite(channelID, 'WriteKey', writeKey, 'Fields', 1, 'Values', 1);
 output1 = thingSpeakRead(channelID, 'ReadKey', readKey, 'Fields', 1)
 
@@ -17,3 +20,14 @@ output3 = thingSpeakRead(channelID, 'ReadKey', readKey, 'Fields', 1)
 
 %value is perminately scrubbed
 output4 = thingSpeakRead(channelID, 'ReadKey', readKey, 'Fields', [1,2])
+
+thingSpeakWrite(channelID, 'WriteKey', writeKey, 'Fields', 1, 'Values', 3);
+pause(1)
+thingSpeakWrite(channelID, 'WriteKey', writeKey, 'Fields', 2, 'Values', 3);
+output5 = thingSpeakRead(channelID, 'ReadKey', readKey, 'Fields', [1,2])
+
+pause(1)
+thingSpeakWrite(channelID, 'WriteKey', writeKey, 'Fields', 1, 'Values', 1);
+pause(1)
+thingSpeakWrite(channelID, 'WriteKey', writeKey, 'Fields', 2, 'Values', 1);
+output6 = thingSpeakRead(channelID, 'ReadKey', readKey, 'Fields', [1,2])
